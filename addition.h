8095ae4ec2874ce9af52754459ef8c83c6cbf906
xiejiawei <pqjrkwem@gmail.com>
2023-12-21 23:01:03 +0800

    ADD::comment

--- addition.h
+++ addition.h
@@ -6,6 +6,15 @@
 #define LEARN_C_ADDITION_H
 
 
+/**
+ * @brief Adds two integers.
+ *
+ * This function takes two integers as input and returns their sum.
+ *
+ * @param a The first integer.
+ * @param b The second integer.
+ * @return The sum of the two integers.
+ */
 int add(int a, int b);
 
 
